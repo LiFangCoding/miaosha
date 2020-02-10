ba8f0cb47491ffe870af5f618c901fb95a0a59be
LiFangCoding <fanglihust@gmail.com>
2020-02-10 16:36:26 +0900

    add validlator

--- src/main/java/com/coding/miaosha/service/impl/UserServiceImpl.java
+++ src/main/java/com/coding/miaosha/service/impl/UserServiceImpl.java
@@ -8,6 +8,8 @@ import com.coding.miaosha.error.BusinessException;
 import com.coding.miaosha.error.EmBusinessError;
 import com.coding.miaosha.service.UserService;
 import com.coding.miaosha.service.model.UserModel;
+import com.coding.miaosha.validator.ValidationResult;
+import com.coding.miaosha.validator.ValidatorImpl;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,9 @@ public class UserServiceImpl implements UserService {
 
   @Autowired
   private UserPasswordDOMapper userPasswordDOMapper;
+
+  @Autowired
+  private ValidatorImpl validator;
 
   @Override
   public UserModel getUserById(Integer id) {
@@ -42,8 +47,9 @@ public class UserServiceImpl implements UserService {
       throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR);
     }
 
-    if (StringUtils.isEmpty(userModel.getName()) || userModel.getGender() == null || userModel.getAge() == null || StringUtils.isEmpty(userModel.getTelephone())) {
-      throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR);
+    ValidationResult result = validator.validate(userModel);
+    if (result.isHasErrors()) {
+      throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR, result.getErrMsg());
     }
 
     UserDO userDO = convertFromModel(userModel);
--- src/main/java/com/coding/miaosha/service/model/UserModel.java
+++ src/main/java/com/coding/miaosha/service/model/UserModel.java
@@ -3,15 +3,31 @@ package com.coding.miaosha.service.model;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
 public class UserModel {
   private Integer id;
+  @NotBlank(message = "Use name cannot be empty")
   private String name;
+
+  @NotNull(message = "gender need filled")
   private String gender;
+
+  @NotNull(message = "age need filled")
+  @Min(value = 0, message = "age must be larger thant 0")
+  @Max(value = 150, message = "age must be smaller thant 150")
   private Integer age;
+
+  @NotBlank(message = "telephone cannot be empty")
   private String telephone;
   private String registerMode;
   private String thirdPartyId;
 
+  @NotBlank(message = "password cannot be empty")
   private String encryptPassword;
 }
